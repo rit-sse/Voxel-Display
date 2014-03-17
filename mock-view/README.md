741b060e8cb293246889320c5637b8eacc842bd2
Lee Avital <leeavital@gmail.com>
2014-03-17 13:16:29 -0400

    Add barebones processing project

---
+++ mock-view/README.md
@@ -0,0 +1,16 @@
+# SBT (scala) project that works with processing libraries
+
+## docs
+http://processing.org/reference/javadoc/core/
+
+## build
+    sbt compile
+
+
+## run
+    sbt run
+
+## dependencies
+* scala
+* sbt
+* Processing Core (found in lib folder)
---
+++ mock-view/src/main/scala/Main.scala
@@ -0,0 +1,36 @@
+import processing.core._
+import javax.swing.JFrame
+
+object Main{
+
+  def main( args : Array[String] ) = {
+    val frame = new JFrame( "Herp" )
+    frame.setSize( 400, 400 )
+    val app = new Embedded()
+    frame.add( app )
+    app.init()
+    frame.setVisible( true )
+  }
+
+}
+
+
+
+class Embedded extends PApplet {
+
+  override def setup() = {
+    // original setup code here ...
+    size(400, 400);
+
+    // prevent thread from starving everything else
+    noLoop();
+  }
+
+  override def draw() = {
+    rect( 40, 40, 20, 20)
+  }
+
+  override def mousePressed() = {
+    // redraw();
+  }
+}
