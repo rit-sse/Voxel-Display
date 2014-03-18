52863da2f91f595b80e429ad3b23b5b39d46651c
JRJurman <j.r.jurman@gmail.com>
2014-03-18 12:02:45 -0400

    Non-working, star code

--- mock-view/src/main/scala/Main.scala
+++ mock-view/src/main/scala/Main.scala
@@ -18,17 +18,39 @@ object Main{
 
 class Embedded extends PApplet {
 
+  var star: PShape = createShape()
+
   override def setup() = {
     // original setup code here ...
     size(400, 400, PConstants.P3D);
 
     // prevent thread from starving everything else
     noLoop();
+
+    star.beginShape()
+    // You can set fill and stroke
+    star.fill(102)
+    star.stroke(255)
+    star.strokeWeight(2)
+    // Here, we are hardcoding a series of vertices
+    star.vertex(0, -50)
+    star.vertex(14, -20)
+    star.vertex(47, -15)
+    star.vertex(23, 7)
+    star.vertex(29, 40)
+    star.vertex(0, 25)
+    star.vertex(-29, 40)
+    star.vertex(-23, 7)
+    star.vertex(-47, -15)
+    star.vertex(-14, -20)
+    star.endShape(PConstants.CLOSE)
   }
 
   override def draw() = {
     fill(155, 0, 0)
-    box( 60, 60, 20 )
+    translate(120, 120, 00)
+    shape(star)
+
     fill(0, 130, 0)
     translate(100, 100, 00)
     box( 60, 60, 20 )
