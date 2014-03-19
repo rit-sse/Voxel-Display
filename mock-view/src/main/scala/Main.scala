38447dfe48d35fff20d3791741d76566ea851aef
JRJurman <j.r.jurman@gmail.com>
2014-03-19 01:43:54 -0400

    voxel and set-voxel methods

--- mock-view/src/main/scala/Main.scala
+++ mock-view/src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 import processing.core._
 import javax.swing.JFrame
+import collection.mutable.HashMap
+import math._
 
 object Main{
 
@@ -18,42 +20,84 @@ object Main{
 
 class Embedded extends PApplet {
 
-  var star: PShape = createShape()
+  val voxelHash = new HashMap[(Float, Float, Float), Boolean]()  { override def default(key:(Float, Float, Float)) = false }
+
 
   override def setup() = {
     // original setup code here ...
-    size(400, 400, PConstants.P3D);
-
+    size(900, 900, PConstants.P3D);
     // prevent thread from starving everything else
     noLoop();
 
-    star.beginShape()
-    // You can set fill and stroke
-    star.fill(102)
-    star.stroke(255)
-    star.strokeWeight(2)
-    // Here, we are hardcoding a series of vertices
-    star.vertex(0, -50)
-    star.vertex(14, -20)
-    star.vertex(47, -15)
-    star.vertex(23, 7)
-    star.vertex(29, 40)
-    star.vertex(0, 25)
-    star.vertex(-29, 40)
-    star.vertex(-23, 7)
-    star.vertex(-47, -15)
-    star.vertex(-14, -20)
-    star.endShape(PConstants.CLOSE)
+
+    //PROGRAM
+    setVoxel( 3, 3, 3 )
+    setVoxel( 3, 3, 4 )
+    setVoxel( 3, 4, 3 )
+    setVoxel( 4, 3, 3 )
   }
 
-  override def draw() = {
-    fill(155, 0, 0)
-    translate(120, 120, 00)
-    shape(star)
+  def voxel(x: Float, y: Float, z: Float, s: Float) = {
+    // FRONT
+    beginShape()
+    vertex(x,   y,   z)       
+    vertex(x+s, y,   z)       
+    vertex(x+s, y+s, z)       
+    vertex(x,   y+s, z)       
+    endShape(PConstants.CLOSE)
 
-    fill(0, 130, 0)
-    translate(100, 100, 00)
-    box( 60, 60, 20 )
+    // BACK
+    beginShape()
+    vertex(x,   y,   z+s)    
+    vertex(x+s, y,   z+s)    
+    vertex(x+s, y+s, z+s)    
+    vertex(x,   y+s, z+s)    
+    endShape(PConstants.CLOSE)
+
+    // SIDE L
+    beginShape()
+    vertex(x,   y,   z)     
+    vertex(x,   y,   z+s)    
+    vertex(x,   y+s, z+s)   
+    vertex(x,   y+s, z)  
+    endShape(PConstants.CLOSE)
+
+    // SIDE R
+    beginShape()
+    vertex(x+s, y,   z)     
+    vertex(x+s, y,   z+s)    
+    vertex(x+s, y+s, z+s)   
+    vertex(x+s, y+s, z)  
+    endShape(PConstants.CLOSE)
+  }
+
+
+  override def draw() = {
+    stroke(0, 0, 0, 0)      // no edges
+    translate(80, 80, 40)   // hard-coded centering
+    val edge = 8
+    for( x <- 1 to edge ) {
+      for ( y <- 1 to edge ) {
+        for ( z <- 1 to edge ) {
+          if ( voxelHash( (x, y, z) ) ) {
+            // This is where we draw opaque ones
+            fill(15, 15, 155, 200)
+          }
+          else {
+            // This is where we draw clear ones
+            fill(15, 15, 15, 5)
+          }
+          voxel(x*20, y*20, z*20, 20)
+        }
+      }
+    }
+  }
+
+  def setVoxel(x: Float, y: Float, z: Float) = {
+    if (voxelHash contains (x, y, z))
+      voxelHash += (x, y, z) -> !voxelHash(x, y, z)
+    else
+      voxelHash += (x, y, z) -> true
   }
 
   override def mousePressed() = {
