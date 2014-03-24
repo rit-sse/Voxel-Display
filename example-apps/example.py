f38f7270782dd16805ae77420762d0163e92e7b1
JRJurman <j.r.jurman@gmail.com>
2014-03-24 16:36:55 -0400

    New python API with example

--- example-apps/example.py
+++ example-apps/example.py
@@ -7,16 +7,16 @@ class MyDisplay(VoxelDisplay):
     def __init__( self ):
         VoxelDisplay.__init__( self )
 
-        self.frameRate =  10
+        self.frameRate =  1
         self.i = 0
 
 
     def draw( self ):
         self.setVoxel( 1, 0, self.i % 5 )
-        #self.setVoxel( 1, 1, self.i % 5 )
-        #self.setVoxel( 1, 2, self.i % 5 )
-        #self.setVoxel( 0, 1, self.i % 5 )
-        #self.setVoxel( 2, 1, self.i % 5 )
+        self.setVoxel( 1, 1, self.i % 5 )
+        self.setVoxel( 1, 2, self.i % 5 )
+        self.setVoxel( 0, 1, self.i % 5 )
+        self.setVoxel( 2, 1, self.i % 5 )
         self.i += 1
 
 
