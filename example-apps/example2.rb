18a8b6ba94704e10951cebb0ba6a204bac380759
JRJurman <j.r.jurman@gmail.com>
2014-03-24 21:27:24 -0400

    Ruby pocket-api and example!

---
+++ example-apps/example2.rb
@@ -0,0 +1,14 @@
+require '../ruby_pocket-api/main'
+
+vd = VoxelDisplay.new()
+
+while true
+  0.upto(7).each do |i|
+    vd.set_voxel( 1, 1, i )
+    vd.toggle_voxel( 7, 7, 7 )
+    vd.flush
+    sleep(0.5)
+  end
+  vd.clear_state
+  vd.flush
+end
