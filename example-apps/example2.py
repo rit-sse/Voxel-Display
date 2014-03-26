dc95cda0b421687aee656666d6431d7b636c38d9
JRJurman <j.r.jurman@gmail.com>
2014-03-26 15:31:15 -0400

    updated python examples, added blinking support to python_pocket-api

--- example-apps/example2.py
+++ example-apps/example2.py
@@ -3,9 +3,15 @@ sys.path.append('../python_pocket-api/')
 from main import VoxelDisplay
 
 import time
-vd = VoxelDisplay()
-for i in range(0, 8):
-    vd.setVoxel( 1, 1, i )
-    vd.toggleVoxel( 7, 7, 7 )
-    vd.flush()
-    time.sleep(0.5)
+vd = VoxelDisplay(True)
+vd.clearState()
+vd.flush()
+while True:
+    vd.toggleBlinkVoxel( 4, 4, 4 )
+    print("a")
+    for i in range(0, 8):
+        print("b")
+        vd.setVoxel( 1, 1, i )
+        vd.flush()
+        time.sleep(0.5)
+    vd.clearState()
--- python_pocket-api/main.py
+++ python_pocket-api/main.py
@@ -3,8 +3,10 @@ import socket
 # This api let's you use the api as an instance of voxel display, not as a class you have to extend
 # This means that YOU have to supply your own loops and threads
 class VoxelDisplay():
-  def __init__ ( self ):
-    self.voxels = []
+  def __init__ ( self, debug=False ):
+    self.voxels = set()
+    self.blinkVoxels = set()
+    self.debug = debug
 
     self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
     self.sock.connect( ("localhost", 8999 ) )
@@ -12,22 +14,35 @@ class VoxelDisplay():
   def flush( self ):
       line = ""
       line = ", ".join([ " ".join( str(i) for i in v) for v in self.voxels ])
-      print(line)
+      if (self.debug):
+        print(line)
       if len(line) > 0:
         self.sock.send( bytes(line  + "\n", 'UTF-8') )
 
+      line = ""
+      line = ", ".join([ " ".join( str(i) for i in v) for v in self.blinkVoxels ])
+      if (self.debug):
+        print('b'+line)
+      if len(line) > 0:
+        self.sock.send( bytes("b"+line  + "\n", 'UTF-8') )
+
   def setVoxel( self, x, y, z ):
-    self.voxels.append( (x,y,z) )
+    self.voxels.add( (x,y,z) )
+
+  def setBlinkVoxel( self, x, y, z ):
+    self.blinkVoxels.add( (x,y,z) )
 
   def toggleVoxel( self, x, y, z ):
-    if (x, y, z) in self.voxels:
-        index = self.voxels.index( (x,y,z) )
-        self.voxels.pop( index )
-    else:
-        self.setVoxel( x, y, z )
+    self.voxels = self.voxels ^ set( [(x,y,z)] )
+
+  def toggleBlinkVoxel( self, x, y, z ):
+    self.blinkVoxels = self.blinkVoxels ^ set( [(x,y,z)] )
 
   def getState( self ):
     return self.voxels
 
-  def clearState( self ):
-    self.voxels = []
+  def clearState( self, normal=True, blink=True ):
+    if (normal):
+        self.voxels = set()
+    if (blink):
+        self.blinkVoxels = set()
